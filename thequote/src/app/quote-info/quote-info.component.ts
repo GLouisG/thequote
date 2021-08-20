444eae57aec758079b2681ba053bc7ab5b142c61
GLouisG <louis.gacho@student.moringaschool.com>
2021-08-20 21:05:13 +0300

    add quote info componenet

---
+++ thequote/src/app/quote-info/quote-info.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import{Quote}from '../quote'
+@Component({
+  selector: 'app-quote-info',
+  templateUrl: './quote-info.component.html',
+  styleUrls: ['./quote-info.component.css']
+})
+export class QuoteInfoComponent implements OnInit {
+
+  @Input() quote! :Quote;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- thequote/src/app/quote/quote.component.ts
+++ thequote/src/app/quote/quote.component.ts
@@ -10,7 +10,7 @@ export class QuoteComponent implements OnInit {
   quotes : Quote[] = [
     new Quote(1, 'Waste no more time arguing about what a good man should be. Be one.', 'Marcus Aurelius', 'Jackie ', new Date(2021,2,2), 0),
     new Quote(2, 'It is not death that a man should fear, but he should fear never beginning to live.', 'Marcus Aurelius', 'Julius Nyongesa ', new Date(2021,3,2), 0),
-    new Quote(3, "It's Not About How Much We've Lost, It is About How Much We Have Left.", 'Tony Stark', 'Julius Nyongesa ', new Date(2021,3,2), 0),
+    new Quote(3, "It's Not About How Much We've Lost, It is About How Much We Have Left.", 'Tony Stark', 'Ken Muriuki', new Date(2021,3,2), 0),
 
   ];
   constructor() { }
