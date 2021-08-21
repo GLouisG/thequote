f8cf7ae0c57c1644c0d639752dc238c83696c5c6
GLouisG <louis.gacho@student.moringaschool.com>
2021-08-21 14:02:38 +0300

    transform function takes in value which is a number

--- thequote/src/app/date-count.pipe.ts
+++ thequote/src/app/date-count.pipe.ts
@@ -5,8 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    
   }
+
+
 
 }
