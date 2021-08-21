9799319fed9b978dfd428846ff0056337cc449f1
GLouisG <louis.gacho@student.moringaschool.com>
2021-08-21 19:08:41 +0300

    add cards for displaying the quotes and change color

--- thequote/src/app/highlighter.directive.ts
+++ thequote/src/app/highlighter.directive.ts
@@ -1,10 +1,11 @@
 import { Directive } from '@angular/core';
+import {Quote} from './quote'
 
 @Directive({
   selector: '[appHighlighter]'
 })
 export class HighlighterDirective {
-
+  
   constructor() { }
 
 }
